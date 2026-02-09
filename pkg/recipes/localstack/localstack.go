bbd83195722d46ad0c9a6e5661f1a6a05f8621cd
Luis Felipe <godoiluisfelipe@gmail.com>
2026-02-08 22:50:00 -0300

    feat: added localstack recipe

---
+++ pkg/recipes/localstack/localstack.go
@@ -0,0 +1,31 @@
+package localstack
+
+import "github.com/luisfelipegodoi/clusterforge/pkg/steps"
+
+type Options struct {
+	Namespace      string
+	ManifestPath   string
+	WaitTimeoutSec int
+}
+
+func Recipe(opts Options) []steps.Step {
+	ns := opts.Namespace
+	if ns == "" {
+		ns = "localstack"
+	}
+	timeout := opts.WaitTimeoutSec
+	if timeout == 0 {
+		timeout = 240
+	}
+
+	return []steps.Step{
+		steps.EnsureNamespace{Namespace: ns},
+		steps.KubectlApply{Namespace: ns, Path: opts.ManifestPath},
+
+		// espera os principais pods ficarem Ready
+		steps.WaitPodsReady{Namespace: ns, Selector: "app.kubernetes.io/name=localstack-server", TimeoutSeconds: timeout},
+		steps.WaitPodsReady{Namespace: ns, Selector: "app.kubernetes.io/name=localstack-repo-server", TimeoutSeconds: timeout},
+		steps.WaitPodsReady{Namespace: ns, Selector: "app.kubernetes.io/name=localstack-redis", TimeoutSeconds: timeout},
+		steps.WaitPodsReady{Namespace: ns, Selector: "app.kubernetes.io/name=localstack-dex-server", TimeoutSeconds: timeout},
+	}
+}
